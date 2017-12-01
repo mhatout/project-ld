ae7ab38221d84e1619f381a4c75f5bba90c8efb6
Muhammad Atout <Muhamad.atout@gmail.com>
2017-12-01 16:53:24 +0200

    ddd

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,6 +1,17 @@
 /*script for project LD*/
 
 $(document).ready(function(){
-        $("#navbar-icon").fadeIn(2000);
-        $(".jumbotron").slideDown(2000);
+        $("#navbar-icon , #navbar button").fadeIn(2000);
+        $("#intro-div").fadeIn(2000);
+        $("#loginbtn").on("click" , function(){
+        	$("#intro-div").hide();
+        	$("#login-div").fadeIn(2000, function(){
+        		$("#loginbtn").hide("slow");
+        	});
+        });
+        $("#submitbtn").on("click" , function(){
+             $("#content-div").fadeIn(2000);
+             $("#login-div").hide();
+        });
+        
 });
