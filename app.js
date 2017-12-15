948cd3ba9dc4ca473736e75537324a08215d18ae
Mohamed M.Ataout <muhamad.atout@gmail.com>
2017-12-15 14:17:50 +0000

    starting backend

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
 
+var vessels = [{name:"MID FORTUNE",number:"542501",status:"issued"},{name:"NICOLAS DELMAS",number:"542502",status:"in process"},
+               {name:"MAERSK TIANJIN",number:"542503",status:"Waiting"}];
 
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine" , "ejs");
+
 
 app.get("/", function(req, res){
     res.render("index");
@@ -13,7 +16,7 @@ app.get("/", function(req, res){
 
 app.post("/table", function(req, res){
     console.log("A request had made to view table");
-   res.render("table"); 
+   res.render("table", {vessels : vessels}); 
 });
 
 app.get("*", function(req, res){
