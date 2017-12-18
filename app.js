1a13065e18ae02b1923f32368e2534e592e4803d
Mohamed M.Ataout <muhamad.atout@gmail.com>
2017-12-18 14:44:29 +0000

    repairing searching problem

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost/project-ld', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+app.set("view engine" , "ejs");
+
 var userSchema = new mongoose.Schema({
     username : String,
     password : Number
@@ -26,9 +30,6 @@ var Vessel = mongoose.model("Vessle" , vesselSchema);
 var vessels = [{name:"MID FORTUNE",number:"542501",status:"issued"},{name:"NICOLAS DELMAS",number:"542502",status:"in process"},
                {name:"MAERSK TIANJIN",number:"542503",status:"Waiting"}];
 
-app.use(express.static("public"));
-app.use(bodyparser.urlencoded({extended:true}));
-app.set("view engine" , "ejs");
 
 
 app.get("/", function(req, res){
@@ -39,16 +40,15 @@ app.get("/", function(req, res){
    res.render("table"); 
 });*/
 
-app.post("/table", function(req, res){
-    console.log("A request had made to view table");
+app.post("/content", function(req, res){
+   console.log(req.body);
    res.render("table", {vessels : vessels}); 
 });
 
-/*app.post("/search", function(req, res){
-    var vessel = req.body.searchinput;
-    console.log(vessel);
-    res.render("table");
-});*/
+app.post("/find", function(req, res){
+    console.log(req.body);
+    res.render("table", {vessels : vessels});
+});
 
 app.get("*", function(req, res){
     res.send("wrong URL!!!");
