f912c5f6f736148aa88ab9ca084aa5e03e7e9310
Mohamed M.Ataout <muhamad.atout@gmail.com>
2017-12-31 11:07:33 +0000

    before Auth

--- app.js
+++ app.js
@@ -1,59 +1,219 @@
-var express = require("express");
-var app = express();
-var bodyparser = require("body-parser");
-var mongoose = require("mongoose");
+var express    = require("express"),
+app            = express(),
+bodyparser     = require("body-parser"),
+mongoose       = require("mongoose"),
+methodOverride = require("method-override");
+
 mongoose.connect('mongodb://localhost/project-ld', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 app.use(express.static("public"));
+app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine" , "ejs");
+app.use(methodOverride("_method"));
 
 var userSchema = new mongoose.Schema({
     username : String,
-    password : Number
+    password : String
 });
 var User = mongoose.model("User" , userSchema);
 
 var vesselSchema = new mongoose.Schema({
     name : String,
-    operation : Number,
-    status : String,
+    operation : String,
+    status : {type:String, default: "Under Progress"},
     supervisor : String,
     purchaser : String,
-    reciever : String,
-    date : Number
+    reciever : '',
+    deliverDate : String,
+    eta : String,
+    created : {type:Date, default: Date.now}
 });
-var Vessel = mongoose.model("Vessle" , vesselSchema);
+var Vessel = mongoose.model("Vessel" , vesselSchema);
 
-
-var vessels = [{name:"MID FORTUNE",number:"542501",status:"issued"},{name:"NICOLAS DELMAS",number:"542502",status:"in process"},
-               {name:"MAERSK TIANJIN",number:"542503",status:"Waiting"}];
-
+var hidden = "hide";
+var visible = "";
+var snackbartxt = "";
+var checked = "";
+var foundVessels = "";
 
 
 app.get("/", function(req, res){
-    res.render("index");
+    
+     Vessel.find({},function(err,vessels){
+        if(err){
+            console.log("Can't retrive data try again later!!");
+        }else{
+            res.render("index", {vessels : vessels, snackbartxt:snackbartxt});
+        }
+    });
 });
 
-/*app.get("/table", function(req, res) {
-   res.render("table"); 
-});*/
+/*---------------- Route to input forms --------------------------- */
 
-app.post("/content", function(req, res){
-   console.log(req.body);
-   res.render("table", {vessels : vessels}); 
+app.post("/form", function(req, res){
+
+ if(req.body.hasOwnProperty("add_vessel")){
+     res.render("form", {vessel_input_visibility : visible, user_input_visibility : hidden});
+  }else{
+     res.render("form", {vessel_input_visibility : hidden, user_input_visibility : visible});}
 });
+
+/*------------------------------------------------------------------*/
+
+/*---------------- User input Route ------------------------------- */
+
+app.post("/add-user", function(req, res){
+   User.create(req.body.user, function(err,user){
+       if(err){
+           snackbartxt="Didn't save please try again later!!!";
+           res.redirect("/");
+       }else{
+           res.redirect("/");
+       }
+   });
+});
+
+/*------------------------------------------------------------------*/
+
+/*---------------- vessel input Route ----------------------------- */
+
+app.post("/add-vessel", function(req, res) {
+   
+   Vessel.create(req.body.vessel, function(err,vessel){
+       if(err){
+           snackbartxt="Didn't save please try again later!!!";
+           res.redirect("/");
+       }else{
+           res.redirect("/");
+       }
+   });
+   
+});
+
+/*----------------------------------------------------------------- */
+
+/*---------------- vessel search Route ---------------------------- */
 
 app.post("/find", function(req, res){
-    console.log(req.body);
-    res.render("table", {vessels : vessels});
+        switch (req.body.searchRadioOptions) {
+                    case ("name"):
+                        Vessel.find({ 'name' : req.body.searchinput},function(err,foundVessels){
+                                if(err){
+                                        console.log("Can't retrive data try again later!!");
+                                }else{
+                                        res.render("index", {vessels : foundVessels, snackbartxt:snackbartxt});
+                                }
+                        });
+                        break;
+                    case ("status"):
+                        Vessel.find({ 'status' : req.body.searchinput},function(err,foundVessels){
+                                if(err){
+                                        console.log("Can't retrive data try again later!!");
+                                }else{
+                                        res.render("index", {vessels : foundVessels, snackbartxt:snackbartxt});
+                                }
+                        });
+                        break;
+                    case ("supervisor"):
+                         Vessel.find({ 'supervisor' : req.body.searchinput},function(err,foundVessels){
+                                if(err){
+                                        console.log("Can't retrive data try again later!!");
+                                }else{
+                                        res.render("index", {vessels : foundVessels, snackbartxt:snackbartxt});
+                                }
+                         });
+                        break;
+                    case ("purchaser"):
+                         Vessel.find({ 'purchaser' : req.body.searchinput},function(err,foundVessels){
+                                if(err){
+                                        console.log("Can't retrive data try again later!!");
+                                }else{
+                                        res.render("index", {vessels : foundVessels, snackbartxt:snackbartxt});
+                                }
+                         });
+                        break;    
+                    default:
+                         Vessel.find({ 'operation' : req.body.searchinput},function(err,foundVessels){
+                                if(err){
+                                        console.log("Can't retrive data try again later!!");
+                                }else{
+                                        res.render("index", {vessels : foundVessels, snackbartxt:snackbartxt});
+                                }
+                         });
+                }
 });
+
+/*----------------------------------------------------------------- */
+
+/*------------------ vessel edit Route ---------------------------- */
+app.get("/:id/edit",function(req, res) {
+
+    Vessel.findById(req.params.id, function(err,foundVessel){
+        if(err){
+            res.redirect("/");
+        }else{
+            res.render("editVessel", {vessel:foundVessel});
+        }
+    });
+    
+});
+
+app.put("/:id/edit-vessel", function(req, res){
+   
+    Vessel.findByIdAndUpdate(req.params.id, req.body.vessel, function(err, ubdatedVessel){
+        if(err){
+            res.redirect("/:id/edit");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/update_status", function(req, res) {
+     Vessel.findByIdAndUpdate(req.body.id, {$set: {"status": req.body.status}}, function(err, ubdatedVessel){
+        if(err){
+            res.redirect("/");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/updateDeliveredVessel", function(req, res) {
+     Vessel.findByIdAndUpdate(req.body.id, {$set: {'status': req.body.status, 'reciever': req.body.reciever, 'deliverDate': req.body.deliverDate }}, function(err, ubdatedVessel){
+        if(err){
+            res.redirect("/");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+/*----------------------------------------------------------------- */
+
+/*------------------ vessel delete Route -------------------------- */
+app.delete("/:id/delete", function(req, res){
+     Vessel.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           alert("Error Occured");
+           res.redirect("/");
+       }else{
+           res.redirect("/");
+       }
+   }); 
+   
+});
+/*----------------------------------------------------------------- */
+
+/*----------------------- Default Route --------------------------- */
 
 app.get("*", function(req, res){
     res.send("wrong URL!!!");
 });
 
+/*----------------------------------------------------------------- */
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started!!");
--- public/scripts/myscript.js
+++ public/scripts/myscript.js
@@ -1,6 +1,12 @@
 /*script for project LD*/
+ 
+ var arrawDown = false;
 
+ 
 $(document).ready(function(){
+    
+        updateVesselStatus();
+        
         $("#navbar-icon , #navbar button").fadeIn(2000);
         $("#intro-div").fadeIn(2000);
         $("#loginbtn").on("click" , function(){
@@ -9,8 +15,246 @@ $(document).ready(function(){
         		$("#loginbtn").hide("slow");
         	});
         });
-});
+        $("#addbtn").on("click" , function(){
+        	$("#btn-div").slideToggle(500);
+        });
+        $(".deleteBtn").on("click", function() {
+                var id = $(".deleteBtn").index(this)+1;
+                $("#deleteModal"+id).modal('show');
+        });
+        $(".editBtn").on("click", function() {
+                var index = $(".editBtn").index(this)+1;
+                $("#editModal"+index).modal('show');
+        });
+        $(".cancelBtn").on("click", function() {
+                $(".ui.mini.basic.modal").modal('hide all'); 
+        });
+	    $(".ui.mini.basic.modal").modal({
+		        closable: true
+     	});
+     
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/     	
+     	
+	$(".preparedCheck").on("click", function() {
+	   var index =  $(".preparedCheck").index(this),
+	       status = "Prepared",
+	       status2= "Under Progress";
+	   
+	  if($(this).is(':checked')){
+	        $.ajax({
+	           url: "/update_status",
+	           data: { 
+                             "id": $("#vesselId"+index).text(), 
+                             "status": status
+                          },
+                   type: "POST",
+                   success: function(response) {
+                            $("#statusCell"+index).text(status);
+                   },
+                   error: function(xhr) {
+                           alert("Something went wrong.. Try again later..");
+                   }
+	          }); // end of ajax
+	         }else {
+	                 
+	          $.ajax({
+	           url: "/update_status",
+	           data: { 
+                             "id": $("#vesselId"+index).text(), 
+                             "status": status2 
+                          },
+                   type: "POST",
+                   success: function(response) {
+                            $("#statusCell"+index).text(status2);
+                   },
+                   error: function(xhr) {
+                           alert("Something went wrong.. Try again later..");
+                   }
+	           }); // end of ajax
+	          } // end of else
+	}); // end of on.click metode
+	
+/*------------------------------------------------------------------------------------------------------------------------------------------*/	
+	
+	$("#toggleRadioBtn").on("click", function(){
+	        if(!arrawDown){
+	                $("#toggleRadioBtn").html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+	              //  arrawDown =true;
+	        }else{
+	                $("#toggleRadioBtn").html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+	               // arrawDown =false;
+	        }
+	        arrawDown =!arrawDown;
+	        $("#radioBtn-div").slideToggle(500);
+	});// end of on.click metode
+	
+/*-----------------------------------------------------------------------------------------------------------------------------------------*/	
+	
+	$(".showDeliverFormBtn").on("click", function() {
+	   var i =  $(".showDeliverFormBtn").index(this);
+	   $(".deliveredBtn").addClass("FirstDelivery");
+	   $(".deliveredBtn").removeClass("additionDelivery");
+	   $("#deliveryForm"+i).slideToggle(500);
+	   
+	}); // end of on.click metode
+	
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/	
+	
+	$(".deliveredBtn").on("click", function() {
+	   var i =  $(".deliveredBtn").index(this), 
+	       status = "Delivered",
+	       recieverval = $("#inputRecipient"+i).val();
+	       
+	     
+	 if($(this).hasClass("FirstDelivery")){   
+    
+	   if(recieverval.trim()){    
+	   $.ajax({
+	           url: "/updateDeliveredVessel",
+	           data: { 
+                             "id": $("#vesselId"+i).text(),
+                             "reciever": recieverval, 
+                             "status": status,
+                             "deliverDate":getDate()
+                          },
+                   type: "POST",
+                   success: function(response) {
+                            $("#statusCell"+i).text(status);
+                            $("#inputRecipient"+i).val("");
+	                        $("#deliveryForm"+i).slideUp(500);
+	                        $("#recieverSpan"+i).removeClass('hide');
+	                        $("#recieverField"+i).html(recieverval);
+	                        $("#etaSpan"+i).addClass("hide");
+	                        $("#deliverDateSpan"+i).removeClass("hide");
+	                        $("#deliverDateField"+i).html(getDate());
+	                        $("#preparedCheck"+i).addClass("hide");
+	                        $("#preparedText"+i).addClass("hide");
+	                        $("#additionBtn"+i).removeClass("hide");
+	                        $("#editBtn"+i).addClass("disabled");
+	                        $("#showDeliverFormBtn"+i).attr("disabled", true);
+	                        $("#showDeliverFormBtn"+i).html('<i class="fa fa-handshake-o" aria-hidden="true"></i> Delivered');
+	                        $(".deliveredBtn").removeClass("FirstDelivery");
+                   },
+                   error: function(xhr) {
+                           alert("Something went wrong.. Try again later..");
+                   }
+	          }); // end of ajax.
+	   } // end of if statment.
+	 }else if($(this).hasClass("additionDelivery")){
+	     
+	     if(recieverval.trim()){
+	         var reciever = $("#recieverField"+i).text()+' , '+$("#inputRecipient"+i).val();
+             $.ajax({
+	                  url: "/updateDeliveredVessel",
+	                  data: { 
+                            "id": $("#vesselId"+i).text(),
+                            "reciever": reciever, 
+                            "status": status,
+                            "deliverDate":getDate()
+                      },
+                      type: "POST",
+                      success: function(response) {
+                            $("#additionBtn"+i).html('<i class="fa fa-plus" aria-hidden="true"></i> Add');
+                            $("#statusCell"+i).text(status);
+                            $("#inputRecipient"+i).val("");
+                            $("#deliveryForm"+i).slideUp(500);
+                            $("#recieverField"+i).html(reciever);
+                            $("#deliverDateField"+i).html(getDate());
+                            $("#additionBtn"+i).attr("disabled", true);
+                            $(".deliveredBtn").removeClass("additionDelivery");
+                      },
+                      error: function(xhr) {
+                            alert("Something went wrong.. Try again later..");
+                      }
+	         }); // end of ajax.
+	     }   
+	 } // end of else if statment.
+	 
+	}); // end of on.click metode.
+	
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+
+	$(".additionBtn").on("click", function() {
+	    var i =  $(".additionBtn").index(this),
+	        btnText = $(this).text();
+	        console.log(btnText);
+	        switch (btnText) {
+	                    case (" Deliver"):
+	                           $(".deliveredBtn").addClass("additionDelivery");
+	                           $(".deliveredBtn").removeClass("firstDelivery");
+	                           $("#deliveryForm"+i).slideToggle(500);
+                        break;
+                        
+                        default:
+                          var status2 = "Addition";
+	                      
+	                      $.ajax({
+	                               url: "/update_status",
+	                               data: {
+                                            "id": $("#vesselId"+i).text(), 
+                                            "status": status2
+                                   },
+                                   type: "POST",
+                                   success: function(response) {
+                                        $("#additionBtn"+i).html('<i class="fa fa-plus" aria-hidden="true"></i> Deliver');
+                                        $("#statusCell"+i).text(status2);
+                                   },
+                                   error: function(xhr) {
+                                        alert("Something went wrong.. Try again later..");
+                                   }
+	                      }); // end of ajax
+	       } // end of switch 
+	}); // end of on.click metode
+	
+}); // end of on.ready metode
+
+/*---------------------------------------------------------------------------*/
 
 function showSnackbar(){
 	$('#snackbar').fadeIn(700).delay(2000).fadeOut(700);
 }
+
+
+function updateVesselStatus(){
+        var checkboxes = $(".preparedCheck"),
+            reciever = $("#inputRecipient"+i).val();
+        for(var i=0; i<checkboxes.length; i++){
+              if($("#statusCell"+i).text() === "Prepared"){
+                  $("#preparedCheck"+i).attr("checked", true);
+              }else if($("#statusCell"+i).text() === "Delivered"){
+	              $("#recieverSpan"+i).removeClass('hide');
+                  $("#etaSpan"+i).addClass("hide");
+	              $("#deliverDateSpan"+i).removeClass("hide");;
+	              $("#preparedCheck"+i).addClass("hide");
+	              $("#preparedText"+i).addClass("hide");
+	              $("#additionBtn"+i).removeClass("hide");
+	              $("#editBtn"+i).addClass("disabled");
+	              $("#showDeliverFormBtn"+i).attr("disabled", true);
+	              $("#showDeliverFormBtn"+i).html('<i class="fa fa-handshake-o" aria-hidden="true"></i> Delivered');
+              }else if($("#statusCell"+i).text() === "Addition"){
+	              $("#recieverSpan"+i).removeClass('hide');
+                  $("#etaSpan"+i).addClass("hide");
+	              $("#deliverDateSpan"+i).removeClass("hide");
+	              $("#preparedCheck"+i).addClass("hide");
+	              $("#preparedText"+i).addClass("hide");
+	              $("#additionBtn"+i).removeClass("hide");
+	              $("#editBtn"+i).addClass("disabled");
+	              $("#showDeliverFormBtn"+i).attr("disabled", true);
+	              $("#showDeliverFormBtn"+i).html('<i class="fa fa-handshake-o" aria-hidden="true"></i> Delivered');
+	              $("#additionBtn"+i).html('<i class="fa fa-plus" aria-hidden="true"></i> Deliver');
+        }
+}
+}
+
+function getDate(){
+    var today = new Date();
+    return(today.toDateString());
+   /* var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+        if(dd<10) {dd='0'+dd;}
+        if(mm<10) {mm='0'+mm;} 
+        today = dd+'/'+mm+'/'+yyyy;
+    return today;*/
+}
+ 
